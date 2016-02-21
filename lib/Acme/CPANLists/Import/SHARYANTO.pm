501eec0d0b045c2526f09bac4d8f37fa5609a7eb
perlancar (@pc-home) <perlancar@gmail.com>
2016-02-21 17:32:33 +0700

    Initial commit

---
+++ lib/Acme/CPANLists/Import/SHARYANTO.pm
@@ -0,0 +1,16 @@
+package Acme::CPANLists::Import::SHARYANTO;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CPANLists imported from Steven Haryanto's blog posts
+
+=head1 DESCRIPTION
+
+
+=head1 append:SEE ALSO
+
+L<App::lcpan> and L<lcpan>, particularly the B<related-mods> subcommand.
+
+Other C<Acme::CPANLists::Import::*> distributions.
